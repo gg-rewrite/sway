69195c9dbdd921974f368d2b88ffb4f37832814a
Some Chinese Guy <freemancrowbar100500@gmail.com>
2019-08-14 17:46:26 +0300

    documented touch functions

--- include/sway/input/touch_gestures.h
+++ include/sway/input/touch_gestures.h
@@ -8,6 +8,10 @@
 #define HYSTERESIS_MM 20
 #define LONG_TAP_MS 500
 
+/** 
+ * a list of gesture types/states
+ *
+ */
 enum touch_gesture_states {
 	TAP,
 	LONG_TAP,
@@ -21,8 +25,8 @@ enum touch_gesture_states {
 };
 
 /**
-represents a single touch point to be stored by sway_cursor 
-for processing multitouch gestures
+ * represents a single touch point to be stored by sway_cursor 
+ * for processing multitouch gestures
  */
 struct sway_touch_point {
 	struct wl_list link;
@@ -35,6 +39,11 @@ struct sway_touch_point {
 	double initial_distance;
 };
 
+/**
+ * represents a single touch gesture holding all the touch points, 
+ * initial and neighboring points for pinch zoom detection and
+ * calculated motion hysteresis based on the assigned output's DPM
+ */
 struct sway_touch_gesture {
 	struct wl_list touch_points;
 	int32_t initial_touch_id;
@@ -44,17 +53,33 @@ struct sway_touch_gesture {
 	enum touch_gesture_states gesture_state;
 };
 
+/**
+ * returns true if hysteresis isn't calculated yet. Shouldn't be needed
+ * once I understand how to get output's parameters in sway_cursor_create
+ */
 bool is_touch_motion_hysteresis_unset(struct sway_touch_gesture *gesture);
 
+/**
+ * calculates hysteresis in px based on DPM and hysteresis in mm
+ */
 void set_touch_motion_hysteresis(struct sway_touch_gesture *gesture,
 		int32_t phys_size,
 		int32_t px_size);
 
+/**
+ * creates and initializes a single gesture
+ */
 struct sway_touch_gesture *touch_gesture_create();
+
+/**
+ * destroys the gesture 
+ */
 void touch_gesture_destroy(struct sway_touch_gesture *gesture);
 
-/**returns true if allowed to proceed with touch passthrough,
-false otherwise
+/**
+ * registers touch points, fills in the initial point.
+ * returns true if allowed to proceed with touch passthrough,
+ * false otherwise
  */
 bool process_touch_down(struct sway_touch_gesture *gesture,
 		int32_t touch_id,
@@ -63,10 +88,16 @@ bool process_touch_down(struct sway_touch_gesture *gesture,
 		uint32_t time_msec,
 		struct wlr_surface *surface);
 
+/**
+ * registers released points, calculates the final gesture to send to processing
+ */
 void process_touch_up(struct sway_touch_gesture *gesture,
 		int32_t touch_id,
 		uint32_t time_msec);
 
+/**
+ * registers touch motion, detects swipe and pinch zoom
+ */
 bool process_touch_motion(struct sway_touch_gesture *gesture,
 		double layout_x,
 		double layout_y,
